e9b732873ef02e8b4ce70b8c7b150fadbfa4cea3
Igor Diogo <igordiogo1314@gmail.com>
2025-03-05 16:24:49 -0300

    Porcentagem

--- app.js
+++ app.js
@@ -21,3 +21,24 @@ function operacaoBasica(entryId){
     resultado = eval(operacao);
     entry.value = resultado;
 }
+
+function porcentagem(tipo, valor1Id, valor2Id, resultadoId){
+    var valor1 = document.querySelector(valor1Id).value;
+    var valor2 = document.querySelector(valor2Id).value;
+    var textoResultado = document.querySelector(resultadoId);
+
+    
+    if(tipo=='porcao'){
+        var resultado = valor2*(valor1/100);
+    }
+    if(tipo=='porcentagem'){
+        var resultado = valor1/(valor2)*100+"%";
+    }
+    if(tipo=='aumento'){
+        var resultado = parseInt(valor2)+(valor2*(valor1/100));
+    }
+    if(tipo=='desconto'){
+        var resultado = parseInt(valor2)-(valor2*(valor1/100));
+    }
+    textoResultado.innerHTML = resultado;
+}
