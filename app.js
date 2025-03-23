e98027bb5e863a8f27dbcb76b96e4da073c00307
Igor Diogo <igordiogo1314@gmail.com>
2025-03-23 18:00:14 -0300

    Calculo de mdc

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
+//inseri caractere no entry
 function inserirCaractere(caractere, entryId){
     var entry = document.querySelector(entryId);
 
     entry.value = entry.value+caractere;
 }
+
+//deleta um caractere
 function deletarCaractere(entryId){
     var entry = document.querySelector(entryId);
 
     entry.value = entry.value.slice(0,-1);
 }
+
+//deleta tudo
 function deletarTodosCaractere(entryId){
     var entry = document.querySelector(entryId);
 
     entry.value = '';
 }
 
+//operações de soma, subtração, multiplicação, divisão e potencia
 function operacaoBasica(entryId){
     var entry = document.querySelector(entryId);
     var operacao = document.querySelector(entryId).value;
 
+    //calcula a operação
     resultado = eval(operacao);
     entry.value = resultado;
 }
@@ -27,16 +34,19 @@ function porcentagem(tipo, valor1Id, valor2Id, resultadoId){
     var valor2 = document.querySelector(valor2Id).value;
     var textoResultado = document.querySelector(resultadoId);
 
-    
+    //calcula a porção de um total
     if(tipo=='porcao'){
         var resultado = valor2*(valor1/100);
     }
+    //calcular a porcentagem de uma porção em relação a um total
     if(tipo=='porcentagem'){
         var resultado = valor1/(valor2)*100+"%";
     }
+    //aumenta x% de um total
     if(tipo=='aumento'){
         var resultado = parseInt(valor2)+(valor2*(valor1/100));
     }
+    //desconta x% de um total
     if(tipo=='desconto'){
         var resultado = parseInt(valor2)-(valor2*(valor1/100));
     }
@@ -46,44 +56,59 @@ function porcentagem(tipo, valor1Id, valor2Id, resultadoId){
 function mmc_mdc(tipo, valoresId, textoResultadoId){
     var textoResultado = document.querySelector(textoResultadoId)
     const valores = document.querySelector(valoresId).value .split(',');
+    //resultado é 1, pois vai ser múltiplicado
     var resultado =  1;
     const listaMultiplos = [];
-    
-    if(tipo=='mmc'){
-        var multiplo = 2;
-        var qtdValores = valores.length;
-        var somaValores = 0;
-        var somarMultiplo=true;
-        var multiploNaoFoiAdicionado = true;
-        while(true){   
-            somarMultiplo=true;
-            somaValores=0;
-            multiploNaoFoiAdicionado = true;
-            for(let i=0;i<qtdValores;i++){
-                valor = parseInt(valores[i])
-                somaValores += valor;
-                console.log(valor);
-                console.log(multiplo);
-                if(valor%multiplo==0){
-                    console.log('é divisivel');
-                    if(multiploNaoFoiAdicionado){
-                        listaMultiplos.push(multiplo);
-                        multiploNaoFoiAdicionado=false;
-                    }
-                    valores[i] = valor/multiplo;
-                    somarMultiplo = false;
-                    console.log(valores[i]);
+
+    //valor começa com 2 e aumenta + 1 se não for divisível por ninguém
+    var multiplo = 2;
+    var qtdValores = valores.length;
+    //somaValores é a soma de todos os valores, quando todos os valores forem 1 a soma deles vai ser == a quantidade de valores
+    var somaValores = 0;
+    //caso o múltiplo não for divisível por nenhum valor ele é somado +1
+    var somarMultiplo=true;
+    //evita a repetição do mesmo múltiplo caso ele seja divisível por mais de um valor(apenas para mmc)
+    var multiploNaoFoiAdicionado = true;
+    //conta quantas vezes um multiplo dividiu, se for mútiplo de todos os valores ele é salvo(apenas para mdc)
+    var vezesMultiplicado = 0;
+
+    while(true){   
+        vezesMultiplicado=0;
+        somarMultiplo=true;
+        somaValores=0;
+        multiploNaoFoiAdicionado = true;
+        for(let i=0;i<qtdValores;i++){
+            valor = parseInt(valores[i])
+            somaValores += valor;
+            console.log(valor);
+            console.log(multiplo);
+            if(valor%multiplo==0){
+                console.log('é divisivel');
+                if(multiploNaoFoiAdicionado && tipo=='mmc'){
+                    listaMultiplos.push(multiplo);
+                    multiploNaoFoiAdicionado=false;
                 }
+                else if(tipo=='mdc'){
+                    vezesMultiplicado++;
+                }
+                valores[i] = valor/multiplo;
+                somarMultiplo = false;
+                console.log(valores[i]);
             }
-            if(somarMultiplo){
-                multiplo++;
-            }
-            if(somaValores==qtdValores){
-                break;
+            if(vezesMultiplicado==qtdValores){
+                listaMultiplos.push(multiplo);
             }
         }
-        console.log(listaMultiplos);
+        if(somarMultiplo){
+            multiplo++;
+        }
+        if(somaValores==qtdValores){
+            break;
+        }
     }
+    console.log(listaMultiplos);
+
+    //multiplica os múltiplos
     for(const valor of listaMultiplos){
         resultado = resultado*valor;
     }
